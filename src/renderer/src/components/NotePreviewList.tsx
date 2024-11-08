99f01c25362f32fd4482811d68fba9ae844a161b
Levy <golvwei@gmail.com>
2024-11-08 22:59:16 +0800

    feat: add empty note screen

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,10 +1,19 @@
 import { notesMock } from '@/store/mocks'
+import { cn } from '@renderer/utils'
 import { ComponentProps } from 'react'
 import { NotePreview } from './NotePreview'
 
-export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
+export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  if (notesMock.length === 0) {
+    return (
+      <ul className={cn('text-center pt-4', className)} {...props}>
+        <span>No Notes Yet!</span>
+      </ul>
+    )
+  }
+
   return (
-    <ul {...props}>
+    <ul className={className} {...props}>
       {notesMock.map((note) => (
         <NotePreview key={note.title + note.lastEditTime} {...note} />
       ))}
