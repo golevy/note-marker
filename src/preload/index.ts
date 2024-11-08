935da964140231628bcb30b81407f80d660846a0
Levy <golvwei@gmail.com>
2024-11-08 22:41:33 +0800

    feat: add localization

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -6,7 +6,7 @@ if (!process.contextIsolated) {
 
 try {
   contextBridge.exposeInMainWorld('context', {
-    // TODO: expose functions here
+    locale: navigator.language
   })
 } catch (error) {
   console.error(error)
